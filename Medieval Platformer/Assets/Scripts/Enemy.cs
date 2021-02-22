b5bebd305c0673440d9d8569bec0d49960177be8
Alexander Pagh <22.alexander.pagh@ksd.org>
2021-02-22 10:01:45 -0800

    Added a bunch of scripts, folders, and place holder objects in the unity project.

---
+++ Medieval Platformer/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float enemyMoveSpeed = 1f;
+
+    Rigidbody2D myRigidBody;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+
+    }
+}
---
+++ Medieval Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float playerRunSpeed = 1f;
+    [SerializeField] float playerJumpSpeed = 1f;
+
+    bool isAlive = true;
+
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider2D;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
+    }
+
+    
+    void Update()
+    {
+        if (!isAlive) { return; }
+        Run();
+        Jump();
+        Attack();
+        Die();
+        TurnAround();
+
+        
+    }
+
+    private void Run()
+    {
+
+
+    }
+
+
+    private void Jump()
+    {
+
+
+    }
+
+    private void Attack()
+    {
+
+
+    }
+
+    private void Die()
+    {
+
+
+    }
+
+    private void TurnAround()
+    {
+
+
+    }
+}
